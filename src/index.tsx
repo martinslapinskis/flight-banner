7941c771133f78e6d91ffcb9ffae1a8ba4734da8
ML <martins.lapinskis@gmail.com>
2021-05-24 20:00:17 +0300

    init

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+import {FFF} from '@components/card';
+
+const App = () => {
+  console.log(FFF)
+  return <View style={{backgroundColor: 'red', width: 100, height: 100}} />;
+};
+
+export default App;
